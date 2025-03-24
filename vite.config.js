f24f67f8c130d2b858680c385e2714f711d22f7b
Mousin Mokaddemi <mousinmokaddemi@MacBook-Air-de-Mousin.local>
2025-03-24 17:55:15 +0100

    Corrigiendo ruta del script

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5001, // Cambiar el puerto a 5001
-    strictPort: true, // Evita que Vite use otro puerto si 5001 está ocupado
-    host: true, // Permite el acceso desde la red
+  build: {
+    outDir: 'dist',
   },
 });
